2cb933584ffce7792db21f4cd4a2f360ca54a1a4
DIVYASREE1610 <me23btech11056@iith.ac.in>
2025-08-06 23:04:36 +0530

    Frontend Completed

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthLayout from '../../components/layouts/AuthLayout';
+import {Link, useNavigate} from 'react-router-dom';
+import Input from '../../components/Inputs/input';
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+
+  const navigate = useNavigate();
+  
+  //Handle Login Form Submit
+  const handleLogin = async (e) => {
+  e.preventDefault();
+
+  if (!validateEmail(email)) {
+    setError("Please enter a valid email address.");
+    return;
+  }
+
+  if (!password) {
+    setError("Please enter the password");
+    return;
+  }
+
+  setError("");
+
+  // Login API Call
+};
+
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
@@ -9,6 +38,37 @@ const Login = () => {
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           Please enter your details to log in
         </p>
+        <form onSubmit={handleLogin }>
+          <Input
+          value={email}
+          onChange={({target}) => setEmail(target.value)}
+          label = "Email Address"
+          placeholder='chinniminnu1116@gmail.com'
+          type='text'
+          />
+
+          <Input
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          label = "Password"
+          placeholder='Min 8 Characters'
+          type='password'
+          />
+          
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            LOGIN
+          </button>
+          
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don’t have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/signup">
+              SignUp
+            </Link>
+          </p>
+
+        </form>
       </div>
     </AuthLayout>
   );
