81b2769177933d6f1ded94978583f01fc52fce31
DIVYASREE1610 <me23btech11056@iith.ac.in>
2025-08-09 20:41:36 +0530

    fixed errors

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,4 +1,4 @@
-import axios  from "axios";
+import axios from "axios";
 import { BASE_URL } from "./apiPaths";
 
 const axiosInstance = axios.create({
@@ -13,28 +13,24 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
     (config) => {
         const accessToken = localStorage.getItem("token");
-        if(accessToken){
+        if (accessToken) {
             config.headers.Authorization = `Bearer ${accessToken}`;
         }
         return config;
     },
-    (error) => {
-        return Promise.reject(error);
-    }
+    (error) => Promise.reject(error)
 );
 
 axiosInstance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
+    (response) => response,
     (error) => {
-        if(error.response){
-            if(error.response.status === 401){
+        if (error.response) {
+            if (error.response.status === 401) {
                 window.location.href = "/login";
-            }else if(error.response.status === 500){
+            } else if (error.response.status === 500) {
                 console.error("Server error. Please try again later.");
             }
-        }else if(error.code === "ECONNABORATED"){
+        } else if (error.code === "ECONNABORTED") {
             console.error("Request timeout. Please try again.");
         }
         return Promise.reject(error);
