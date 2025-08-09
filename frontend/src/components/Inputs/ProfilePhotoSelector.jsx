caef1d7155eedade2bda84c31cbfc272283205a1
DIVYASREE1610 <me23btech11056@iith.ac.in>
2025-08-09 18:57:54 +0530

    Fixed errors

--- frontend/src/components/Inputs/ProfilePhotoSelector.jsx
+++ frontend/src/components/Inputs/ProfilePhotoSelector.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { LuUser, LuUpload, LuTrash} from 'react-icons/lu';
+import { LuUser, LuUpload, LuTrash } from 'react-icons/lu';
 
 const ProfilePhotoSelector = ({ image, setImage }) => {
   const inputRef = useRef(null);
@@ -8,10 +8,7 @@ const ProfilePhotoSelector = ({ image, setImage }) => {
   const handleImageChange = (event) => {
     const file = event.target.files[0];
     if (file) {
-      // Update the image state
       setImage(file);
-
-      // Generate preview URL from the file
       const preview = URL.createObjectURL(file);
       setPreviewUrl(preview);
     }
@@ -28,43 +25,41 @@ const ProfilePhotoSelector = ({ image, setImage }) => {
 
   return (
     <div className="flex justify-center mb-6">
-        <input
-      type="file"
-      accept="image/*"
-      ref={inputRef}
-      onChange={handleImageChange}
-      className="hidden"
-    />
+      <input
+        type="file"
+        accept="image/*"
+        ref={inputRef}
+        onChange={handleImageChange}
+        className="hidden"
+      />
 
-    {!image ? (
-      <div className="w-20 h-20 flex items-center justify-center bg-purple-100 rounded-full relative">
-        <LuUser className="text-4xl text-primary" />
-
-        <button
-          type="button"
-          className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full absolute -bottom-1 -right-1"
-          onClick={onChooseFile}
-        >
-          <LuUpload />
-        </button>
-      </div>
-    ) : (
-      <div className="relative">
-    <img
-    src={previewUrl}
-    alt="profile photo"
-    className="w-20 h-20 rounded-full object-cover"
-    />
-
-    <button
-    type="button"
-    className="w-8 h-8 items-center justify-center bg-red-500 text-white rounded-full -bottom-1 -right-1"
-    onClick={handleRemoveImage}
-    >
-        <LuTrash />
-        </button>
-    </div>
-    )}
+      {!image ? (
+        <div className="w-20 h-20 flex items-center justify-center bg-purple-100 rounded-full relative">
+          <LuUser className="text-4xl text-primary" />
+          <button
+            type="button"
+            className="w-8 h-8 flex items-center justify-center bg-primary text-white rounded-full absolute -bottom-1 -right-1"
+            onClick={onChooseFile}
+          >
+            <LuUpload />
+          </button>
+        </div>
+      ) : (
+        <div className="relative w-20 h-20">
+          <img
+            src={previewUrl}
+            alt="profile photo"
+            className="w-full h-full rounded-full object-cover"
+          />
+          <button
+            type="button"
+            className="w-8 h-8 flex items-center justify-center bg-red-500 text-white rounded-full absolute -bottom-1 -right-1"
+            onClick={handleRemoveImage}
+          >
+            <LuTrash />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
